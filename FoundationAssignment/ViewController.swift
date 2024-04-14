16d96ddec722969253554e7e5d06987e16d1d7da
IOS-GURU <114640700+IOS-GURU@users.noreply.github.com>
2024-04-14 18:19:10 +0530

    Created new project
    Created new project for the assessment

---
+++ FoundationAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoundationAssignment
+//
+//  Created by Gaurang Patel on 14/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
